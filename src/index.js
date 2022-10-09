63e3b9715705e265981c95327dc966bee146e701
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-10-09 14:42:33 -0300

    TG: Index and .json creation

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
+const adminRouter = require('./resources/admins');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/admin', adminRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
