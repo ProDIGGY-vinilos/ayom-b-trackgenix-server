85aacc4c786f88d1f06691e81d5d34580e30bf04
cami-diaz <diazcamilae@gmail.com>
2022-10-09 13:26:43 -0300

    TG: Created method to POST and DELETE

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 const projects = require('../data/projects.json');
@@ -10,6 +11,34 @@ router.get('/:id', (req, res) => {
   } else {
     res.status(400).json({ msg: `No project with the id of ${req.params.id}` });
   }
+});
+
+router.delete('/:id', (req, res) => {
+  const projectId = req.params.id;
+  const filterTs = projects.filter((project) => String(project.id) !== projectId);
+  if (projects.length === filterTs.length) {
+    res.send('Could not delete because the time sheet was not found');
+  } else {
+    fs.writeFile('src/data/projects.json', JSON.stringify(filterTs), (err) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        res.status(200).send(filterTs);
+      }
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  const newProject = req.body;
+  projects.push(newProject);
+  fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).json(newProject);
+    }
+  });
 });
 
 module.exports = router;
