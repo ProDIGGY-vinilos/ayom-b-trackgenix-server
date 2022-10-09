17f697e71977b21ab5aefc756f8b40ea93eac410
Julian Flores <julianfloresdev@gmail.com>
2022-10-09 16:15:45 -0300

    TG: Finally create Edit/Add Employee/Get by filter

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable import/no-import-module-exports */
@@ -10,64 +12,105 @@ const projectsList = require('../data/projects.json');
 
 const router = express.Router();
 
-//! Create a new Project.
-router.post('/create', (req, res, next) => {
-  const projectFinalId = projectsList[projectsList.length - 1].id;
-  const isValidProject = req.body.hasOwnProperty('name')
-        && req.body.hasOwnProperty('description')
-        && req.body.hasOwnProperty('startDate')
-        && req.body.hasOwnProperty('endDate')
-        && req.body.hasOwnProperty('clientName');
-  const projectExist = projectsList.some((project) => project.name === String(req.body.name));
-  if (isValidProject && !projectExist) {
-    req.body.id = projectFinalId + 1;
-    projectsList.push(req.body);
-    fs.writeFile('src/data/projects.json', JSON.stringify(projectsList), (err) => {
-      if (err) {
-        return res.status(400)
-          .send('Project invalid! Verify information sent.');
-      }
-      return res.json(projectsList);
-    });
-    res.json(projectsList);
-  } else if (projectExist) {
-    return res.send('You are trying to create an existing project in our database.');
-  } else {
-    return res.status(400)
-      .send('Project invalid! Verify information sent.');
-  }
-  next();
-});
+const findProjectOnReq = (request) => {
+  const checkBody = (property) => request.body.hasOwnProperty(property);
 
-//! Edit Project
-// router.put('/edit/project', (req, res) => {
-//   const projectToEdit = req.body;
-// });
+  const filterProjects = projectsList.filter((project) => (checkBody('id') ? project.id === Number(request.body.id)
+    : checkBody('name') ? project.name === String(request.body.name)
+      : checkBody('clientName') ? project.clientName === String(request.body.clientName)
+        : checkBody('startDate') ? project.startDate === String(request.body.startDate)
+          : checkBody('endDate') ? project.endDate === String(request.body.endDate) : false));
+  return filterProjects;
+};
 
-//! Edit Project to add employee
-router.put('/edit/employee', (req, res) => {
-  const employeeToAdd = req.body.employee;
-  const checkBody = (property) => req.body.hasOwnProperty(property);
-  const isValidProject = projectsList.filter((project) => (checkBody('id') ? project.id === Number(req.body.id)
-    : checkBody('name') ? project.name === String(req.body.name)
-      : checkBody('clientName') ? project.clientName === String(req.body.clientName)
-        : checkBody('startDate') ? project.startDate === String(req.body.startDate)
-          : checkBody('endDate') ? project.endDate === String(req.body.endDate) : false));
-  if (isValidProject.length === 0 || !req.body.employee.hasOwnProperty('rol')) {
-    res.json({
-      msg: 'Project invalid! Verify information sent.',
-    });
-  } else {
-    const projectToEdit = projectsList.find((project) => project.id === Number(req.body.id));
-    projectToEdit.employee.push(employeeToAdd);
+const findEmployeeOnProject = (project, request) => {
+  const employeeMatch = (property) => request.body.employee.hasOwnProperty(property);
+  const arrayOfEmployees = project.employee;
+  const employeeToDelete = arrayOfEmployees.filter((emp) => (employeeMatch('id') ? emp.id === Number(request.body.employee.id)
+    : employeeMatch('name') ? emp.name === String(request.body.employee.name)
+      : false));
+  return employeeToDelete;
+};
+
+/*
+!Edit Project
+? On route must have ID number after /
+? On body must have Property to edit with new value.
+*/
+router.put('/edit/project/:id', (req, res) => {
+  const projectToEdit = projectsList.find((project) => project.id === Number(req.params.id)) || [];
+  if (projectToEdit.length !== 0) {
+    projectToEdit.name = req.body.name ? req.body.name : projectToEdit.name;
+    projectToEdit.clientName = req.body.clientName ? req.body.clientName : projectToEdit.clientName;
+    projectToEdit.description = req.body.description ? req.body.description : projectToEdit.description;
+    projectToEdit.startDate = req.body.startDate ? req.body.startDate : projectToEdit.startDate;
+    projectToEdit.endDate = req.body.endDate ? req.body.endDate : projectToEdit.endDate;
     fs.writeFile('src/data/projects.json', JSON.stringify(projectsList), (err) => {
       if (err) {
         res.send('Cannot edit this project');
       } else {
-        res.json(projectToEdit);
+        res.send('Project successfully updated!')
+          .json(projectToEdit);
+      }
+    });
+  } else {
+    res.send('This is not a valid project ID');
+  }
+});
+
+/*
+!Edit Project to add employee
+? On body must have 2 params:
+? 1) "id" to find the project to edit employee list
+? 2) "employee" must be an object that matches a real employee. Must have "rol" inside (DEV, QA, TL, PM)
+*/
+router.put('/add/employee', (req, res, next) => {
+  const employeeToAdd = req.body.employee;
+  const filterProjects = findProjectOnReq(req);
+  if (filterProjects.length === 0 || !req.body.employee.hasOwnProperty('rol')) {
+    res.send('Project invalid! Verify information sent.');
+  } else {
+    const employeeList = filterProjects[0].employee;
+    const employeeFinalId = employeeList.at(-1).id;
+    employeeToAdd.id = employeeFinalId + 1;
+    filterProjects[0].employee.push(employeeToAdd);
+    fs.writeFile('src/data/projects.json', JSON.stringify(projectsList), (err) => {
+      if (err) {
+        res.send('Cannot edit this project');
+      } else {
+        res.send('Changes Done Successfully')
+          .json(filterProjects);
       }
     });
   }
+  next();
+});
+
+/* Delete employee in projectsList
+? On body must have 2 params:
+? 1) "id" to find the project to edit employee list
+? 2) Employee "name" or "id"
+*/
+router.put('/delete/employee', (req, res, next) => {
+  const filterProjects = findProjectOnReq(req);
+  const employeeToDelete = findEmployeeOnProject(filterProjects[0], req);
+  if (filterProjects.length === 0
+      || !(req.body.employee.hasOwnProperty('id') || req.body.employee.hasOwnProperty('name'))) {
+    res.json({
+      msg: 'Project invalid! Verify information sent.',
+    });
+  } else {
+    filterProjects[0].employee = filterProjects[0].employee.filter((emp) => emp.id !== employeeToDelete.id);
+    fs.writeFile('src/data/projects.json', JSON.stringify(projectsList), (err) => {
+      if (err) {
+        res.send('Cannot edit this project');
+      } else {
+        res.send('Employee Delete Successfully')
+          .json(filterProjects);
+      }
+    });
+  }
+  next();
 });
 
 //! Get projects
@@ -76,17 +119,21 @@ router.get('/getAll', (req, res, next) => {
   next();
 });
 
+/* Get projects by argument
+? On body must have almost 1 params between:
+? "id"
+? "name"
+? "clientName"
+? "startDate"
+? "endDate"
+*/
 router.get('/getByArgument', (req, res, next) => {
-  const checkBody = (property) => req.body.hasOwnProperty(property);
-  const filterProjects = projectsList.filter((project) => (checkBody('id') ? project.id === Number(req.body.id)
-    : checkBody('name') ? project.name === String(req.body.name)
-      : checkBody('clientName') ? project.clientName === String(req.body.clientName)
-        : checkBody('startDate') ? project.startDate === String(req.body.startDate)
-          : checkBody('endDate') ? project.endDate === String(req.body.endDate) : false));
+  const filterProjects = findProjectOnReq(req);
   if (filterProjects.length === 0) {
     res.send('This data dosen\'t match with any project! Please check data entry');
   } else {
-    res.json(filterProjects);
+    res.send('Changes done successfully')
+      .json(filterProjects);
   }
   next();
 });
