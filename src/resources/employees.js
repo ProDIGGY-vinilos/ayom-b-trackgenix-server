c0727a9ecea72bdea1bcdab7eab0a9cc0cba78f3
Emanuel Muises <emanuelmuises95@gmail.com>
2022-10-12 20:13:14 -0300

    TG: corrections PR's feedback done

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -5,7 +5,7 @@ const employees = require('../data/employees.json');
 
 const router = express.Router();
 
-router.get('/getAll', (req, res) => {
+router.get('/', (req, res) => {
   res.send(employees);
 });
 
@@ -15,21 +15,22 @@ router.get('/getById/:id', (req, res) => {
   if (checkEmployee) {
     res.send(checkEmployee);
   } else {
-    res.send('Employee NOT FOUND');
+    res.send(`Employee with the Id ${employeeId} NOT FOUND`);
   }
 });
 
 router.delete('/delete/:id', (req, res) => {
   const employeeId = req.params.id;
-  const findEmployee = employees.find((employee) => employee.id === employeeId);
+  const foundEmployee = employees.find((employee) => employee.id === employeeId);
   const filteredEmployee = employees.filter((employee) => employee.id !== employeeId);
-  if (!findEmployee) {
-    res.send('Employee NOT FOUND');
+  if (!foundEmployee) {
+    res.send(`Employee with the Id ${employeeId} NOT FOUND`);
   } else {
-    res.send(`Employee deleted ${findEmployee.name}`);
     fs.writeFile('src/data/employees.json', JSON.stringify(filteredEmployee), (err) => {
       if (err) {
         res.send('Error trying to delete employee');
+      } else {
+        res.send(`Employee deleted ${foundEmployee.name}`);
       }
     });
   }
