c6617090cda25f996d36e4e33fcfc13f7418597b
cami-diaz <diazcamilae@gmail.com>
2022-10-11 21:13:52 -0300

    TG: Added success message on delete and post

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -4,39 +4,45 @@ const fs = require('fs');
 const router = express.Router();
 const projects = require('../data/projects.json');
 
-router.get('/:id', (req, res) => {
-  const found = projects.find((project) => String(project.id) === req.params.id);
-  if (found) {
-    res.status(200).json(projects.filter((project) => String(project.id) === req.params.id));
+router.get('/getById/:id', (req, res) => {
+  const projectFound = projects.find((project) => String(project.id) === req.params.id);
+  if (projectFound) {
+    res.status(200).json(projectFound);
   } else {
     res.status(400).json({ msg: `No project with the id of ${req.params.id}` });
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
   const projectId = req.params.id;
-  const filterTs = projects.filter((project) => String(project.id) !== projectId);
-  if (projects.length === filterTs.length) {
-    res.send('Could not delete because the time sheet was not found');
+  const projectFiltered = projects.filter((project) => String(project.id) !== projectId);
+  if (projects.length === projectFiltered.length) {
+    res.send('Could not delete because the project was not found');
   } else {
-    fs.writeFile('src/data/projects.json', JSON.stringify(filterTs), (err) => {
+    fs.writeFile('src/data/projects.json', JSON.stringify(projectFiltered), (err) => {
       if (err) {
-        res.status(404).send(err);
+        res.status(400).send({ msg: `Could not delete project: ${err}` });
       } else {
-        res.status(200).send(filterTs);
+        res.status(200).send({
+          msg: 'Project deleted successfully',
+          data: projectFiltered,
+        });
       }
     });
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/create', (req, res) => {
   const newProject = req.body;
   projects.push(newProject);
   fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
     if (err) {
-      res.status(404).send(err);
+      res.status(400).send({ msg: `Could not create project: ${err}` });
     } else {
-      res.status(200).json(newProject);
+      res.status(200).send({
+        msg: 'Project created successfully',
+        data: newProject,
+      });
     }
   });
 });
