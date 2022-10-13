8a2af077fc9f48fd9541db40ad834ab5682ef1af
cami-diaz <diazcamilae@gmail.com>
2022-10-13 20:37:19 -0300

    TG: modifications in task.js

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -4,26 +4,26 @@ const fs = require('fs');
 const router = express.Router();
 const tasks = require('../data/tasks.json');
 
-router.get('/:id', (req, res) => {
-  const found = tasks.find((task) => String(task.id) === req.params.id);
-  if (found) {
+router.get('/getById/:id', (req, res) => {
+  const taskFound = tasks.find((task) => String(task.id) === req.params.id);
+  if (taskFound) {
     res.status(200).json(tasks.filter((task) => String(task.id) === req.params.id));
   } else {
     res.status(400).json({ msg: `No task with the id of ${req.params.id}` });
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
   const tasksId = req.params.id;
-  const filterTs = tasks.filter((task) => String(task.id) !== tasksId);
-  if (tasks.length === filterTs.length) {
-    res.send('Could not delete because the time sheet was not found');
+  const filterTask = tasks.filter((task) => String(task.id) !== tasksId);
+  if (tasks.length === filterTask.length) {
+    res.send('Could not delete because the task was not found');
   } else {
-    fs.writeFile('src/data/tasks.json', JSON.stringify(filterTs), (err) => {
+    fs.writeFile('src/data/tasks.json', JSON.stringify(filterTask), (err) => {
       if (err) {
         res.status(404).send(err);
       } else {
-        res.status(200).send(filterTs);
+        res.status(200).send(filterTask);
       }
     });
   }
