1b34f74e9a1430cc2b62bb852ee2f56e40a422bc
Nicolas Gulam <nicogulam99@gmail.com>
2022-10-13 20:58:37 -0300

    TG: Added Corrections in code

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -6,44 +6,64 @@ const router = express.Router();
 const superAdmins = require('../data/super-admins.json');
 
 // Get Super Admins
-router.get('/getAllSuperAdmins', (req, res) => {
+router.get('/', (req, res) => {
   if (superAdmins) {
     res.send(superAdmins);
   } else {
     res.send('Super Admins not found');
   }
 });
-router.get('/getSuperAdminsById/:id', (req, res) => {
+
+// Get Super Admins by ID
+router.get('/getById/:id', (req, res) => {
   const superAdminId = req.params.id;
   const foundSuperAdmin = superAdmins.find((superAdmin) => superAdmin.id === superAdminId);
   if (foundSuperAdmin) {
     res.send(foundSuperAdmin);
   } else {
-    res.send('Super Admin not found');
+    res.send(`Super admin with the id ${superAdminId} not found`);
   }
 });
+
 // Edit Super Admins
-router.put('/modifySuperAdmins/:id', (req, res) => {
+router.put('/update/:id', (req, res) => {
   let counterData = 0;
-  const foundSA = superAdmins.find((superAdmin) => superAdmin.id === req.params.id);
-  const editSA = req.body;
-  if (!foundSA) {
-    res.send('The user does not exist.');
+  const superAdminId = req.params.id;
+  const foundSuperAdmin = superAdmins.find((superAdmin) => superAdmin.id === req.params.id);
+  const editSuperAdmin = req.body;
+  if (!foundSuperAdmin) {
+    res.send(`Super admin with the id ${superAdminId} not found`);
   } else {
-    foundSA.name === editSA.name ? counterData += 1 : foundSA.name = editSA.name; // eslint-disable-line
-    foundSA.lastName === editSA.lastName ? counterData += 1 : foundSA.lastName = editSA.lastName; // eslint-disable-line
-    foundSA.email === editSA.email ? counterData += 1 : foundSA.email = editSA.email; // eslint-disable-line
-    foundSA.password === editSA.password ? counterData += 1 : foundSA.password = editSA.password; // eslint-disable-line
-    const index = superAdmins.indexOf(foundSA);
-    superAdmins[index] = editSA;
+    if (foundSuperAdmin.name === editSuperAdmin.name) {
+      counterData += 1;
+    } else {
+      foundSuperAdmin.name = editSuperAdmin.name;
+    }
+    if (foundSuperAdmin.lastName === editSuperAdmin.lastName) {
+      counterData += 1;
+    } else {
+      foundSuperAdmin.lastName = editSuperAdmin.lastName;
+    }
+    if (foundSuperAdmin.email === editSuperAdmin.email) {
+      counterData += 1;
+    } else {
+      foundSuperAdmin.email = editSuperAdmin.email;
+    }
+    if (foundSuperAdmin.password === editSuperAdmin.password) {
+      counterData += 1;
+    } else {
+      foundSuperAdmin.password = editSuperAdmin.password;
+    }
+    const index = superAdmins.indexOf(foundSuperAdmin);
+    superAdmins[index] = editSuperAdmin;
     if (counterData === 4) {
       res.send('The new data is the same as the old one');
     } else {
       fs.writeFile('src/data/super-admins.json', JSON.stringify(superAdmins), (err) => {
         if (err) {
-          res.send('Cannot edit the user.');
+          res.send(`Could not edit super admin with id ' ${superAdminId}`);
         } else {
-          res.send('User edited successfully.');
+          res.send(`Super admin updated successfully: ${JSON.stringify(editSuperAdmin, null, 2)}`);
         }
       });
     }
