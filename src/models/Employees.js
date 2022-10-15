13df50f649ccb53b7f54285efe4eba22599d3260
Dino Stampella <dino.stampella96@gmail.com>
2022-10-14 23:11:52 -0300

    TG-10: create employee schema

---
+++ src/models/Employees.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const employeeSchema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: Number, required: true },
+  email: { required: true },
+  password: { required: true },
+});
+
+export default mongoose.model('Employee', employeeSchema);
