5cb94176403f0d937c929e25232cbf87afd0d1db
Dino Stampella <dino.stampella96@gmail.com>
2022-10-18 00:25:14 -0300

    TG-10: correct validation

--- src/validations/employees.js
+++ src/validations/employees.js
@@ -2,18 +2,18 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const employeesValidation = Joi.object({
-    name: Joi.string().required().alphanum().pattern(/^([^0-9]*)$/i, 'only letters'),
-    lastName: Joi.string().required().alphanum().pattern(/^([^0-9]*)$/i, 'only letters'),
+    name: Joi.string().alphanum().pattern(/^([^0-9]*)$/i, 'only letters').required(),
+    lastName: Joi.string().alphanum().pattern(/^([^0-9]*)$/i, 'only letters').required(),
     phone: Joi.string().length(10).pattern(/^[0-9]+$/, 'only numbers').required(),
     email: Joi.string().email().required(),
-    password: Joi.string().alphanum().required(),
+    password: Joi.string().alphanum().pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, 'Letters, numbers and minimum 8 characters').required(),
   });
 
   const validation = employeesValidation.validate(req.body);
 
   if (validation.error) {
     return res.status(400).json({
-      message: `There was an error: ${validation.error.details[0].message}`,
+      message: `There was an error: ${validation.error.message}`,
     });
   }
   return next();
