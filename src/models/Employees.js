4d4f438a2fd48400aa519aaae22686e5e7ebe403
Dino Stampella <dino.stampella96@gmail.com>
2022-10-17 22:19:19 -0300

    TG-10: correct schema, create routes and create validations

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 const employeeSchema = new Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
-  phone: { type: Number, required: true },
-  email: { required: true },
-  password: { required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 export default mongoose.model('Employee', employeeSchema);
---
+++ src/validations/employees.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const validateCreation = (req, res, next) => {
+  const employeesValidation = Joi.object({
+    name: Joi.string().required().alphanum().pattern(/^([^0-9]*)$/i, 'only letters'),
+    lastName: Joi.string().required().alphanum().pattern(/^([^0-9]*)$/i, 'only letters'),
+    phone: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().alphanum().required(),
+  });
+
+  const validation = employeesValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `There was an error: ${validation.error.details[0].message}`,
+      error: true,
+    });
+  }
+  return next();
+};
+export default {
+  validateCreation,
+};
