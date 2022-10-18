3348b1cf005eefad6fd7a52285d7d777f426c416
cami-diaz <diazcamilae@gmail.com>
2022-10-17 23:44:31 -0300

    TG-8: Create delete, update and get by id for task

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -36,7 +36,80 @@ const createNewTask = async (req, res) => {
   }
 };
 
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Tasks.findById({ _id: req.params.id });
+    if (task) {
+      return res.status(200).json({
+        msg: 'Task found succesfully',
+        data: task,
+        error: false,
+      });
+    }
+    return res.status(400).json({
+      msg: `Cannot find task with ID: ${req.params.id}`,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: `There was an error: ${error}`,
+      error: true,
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const taskToDelete = await Tasks.findByIdAndDelete({ _id: req.params.id });
+    if (taskToDelete) {
+      return res.status(200).json({
+        msg: 'Task delete succesfully',
+        data: taskToDelete,
+        error: false,
+      });
+    }
+    return res.status(404).json({
+      msg: `Cannot delete task with ID: ${req.params.id}`,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: `There was an error: ${error}`,
+      error: true,
+    });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const taskToUpdate = await Tasks.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true },
+    );
+    if (taskToUpdate) {
+      return res.status(200).json({
+        msg: 'Taskupdated succesfully',
+        data: taskToUpdate,
+        error: false,
+      });
+    }
+    return res.status(404).json({
+      msg: `Cannot find task with ID: ${req.params.id}`,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: `There was an error: ${error}`,
+      error: true,
+    });
+  }
+};
+
 export default {
   getAllTasks,
   createNewTask,
+  getTaskById,
+  deleteTask,
+  updateTask,
 };
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router
   .get('/', tasksControllers.getAllTasks)
-  .post('/', taskValidation.createValidation, tasksControllers.createNewTask);
+  .post('/', taskValidation.createValidation, tasksControllers.createNewTask)
+  .get('/:id', tasksControllers.getTaskById)
+  .put('/:id', taskValidation.createValidation, tasksControllers.updateTask)
+  .delete('/:id', tasksControllers.deleteTask);
 
 export default router;
