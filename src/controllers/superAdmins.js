8afa10880d22e7375978dc7e73e1f9717c6dc6dc
Nicolas Gulam <nicogulam99@gmail.com>
2022-10-18 23:21:30 -0300

    TG-5: Added Filters in GetAll Function and password validation

--- src/controllers/superAdmins.js
+++ src/controllers/superAdmins.js
@@ -2,11 +2,16 @@ import SuperAdmins from '../models/SuperAdmins';
 
 const getAllSuperAdmins = async (req, res) => {
   try {
-    const superAdmins = await SuperAdmins.find();
-
-    return res.status(200).json({
-      message: 'Super Admin found',
-      data: superAdmins,
+    const superAdmins = await SuperAdmins.find(req.query);
+    if (superAdmins.length) {
+      return res.status(200).json({
+        message: 'Super Admin found',
+        data: superAdmins,
+      });
+    }
+    return res.status(404).json({
+      message: 'Super Admin not found',
+      query: req.query,
     });
   } catch (err) {
     return res.status(400).json({
--- src/validations/superAdmins.js
+++ src/validations/superAdmins.js
@@ -5,14 +5,14 @@ const validateCreation = (req, res, next) => {
     name: Joi.string().alphanum().pattern(/([a-zA-Z])$/i, 'Only letters').required(),
     lastName: Joi.string().alphanum().pattern(/([a-zA-Z])$/i, 'Only letters').required(),
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().alphanum().required(),
+    password: Joi.string().alphanum().pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, 'Letters, numbers and minimum 8 characters').required(),
   });
 
   const validation = superAdminValidation.validate(req.body);
 
   if (validation.err) {
     return res.status(400).json({
-      message: `There was an error: ${validation.error.datails[0].message}`,
+      message: `There was an error: ${validation.err.message}`,
     });
   }
 
