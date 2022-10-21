f407583c304b0e10602c640a36ee4dd31fcd05b7
Julian Flores <julianfloresdev@gmail.com>
2022-10-21 20:37:38 -0300

    TG-29: Create seeds of employee & superAdmin

---
+++ src/seeds/employees.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: mongoose.Types.ObjectId('6352daf070bd974cac6927cc'),
+    name: 'Samir',
+    lastName: 'Sadiki',
+    email: 'sasamirdiki@gmail.com',
+    password: 'fpe2fwe4mv230g',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6352daf6f8285eeb69b2fabe'),
+    name: 'Rhonald',
+    lastName: 'Sierpinsky',
+    email: 'ron.sierp87@gmail.com',
+    password: '2n4120vsds09vwev4',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6352dafc15b1b196950a8583'),
+    name: 'Rita',
+    lastName: 'Milstein',
+    email: 'maju_12puet@hotmail.com',
+    password: '10we43qovls823mf7',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6352dbec0259a3a36db761d3'),
+    name: 'Milthon',
+    lastName: 'Sierra',
+    email: 'pela_king@hotmail.com',
+    password: 'nu1240ff2i1r1n3r013',
+  },
+];
---
+++ src/seeds/superAdmins.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: mongoose.Types.ObjectId('6352d89541876d1b1b99dbf6'),
+    name: 'Ignacio',
+    lastName: 'Aristo',
+    email: 'NoMeDigan_Nacho@gmail.com',
+    password: 'elMrSatanSinRulos',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6352d7e777ae480815a63af2'),
+    name: 'Verona',
+    lastName: 'Baudracco',
+    email: 'ReDesaparecidaLaVero@gmail.com',
+    password: 'NoEsVeronicaEsVERONA',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6352d3e9239ac7eb3f7d3e88'),
+    name: 'Elsa',
+    lastName: 'Pato',
+    email: 'elsa69@hotmail.com',
+    password: 'Cenicienta69',
+  },
+];
