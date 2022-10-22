1fc0b0dde188d0b37e58ad89fe9bcbe51796a370
Guido Gentile <guido.gentile.1995@gmail.com>
2022-10-21 22:08:55 -0300

    TG-28: Added admins seed

---
+++ src/seeds/admins.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: mongoose.Types.ObjectId('63533d49fc13ae16b7000000'),
+    name: 'Erika',
+    lastName: 'Dozdill',
+    email: 'edozdill0@comcast.net',
+    password: 'cB3CxOtq',
+  },
+  {
+    _id: mongoose.Types.ObjectId('63533d49fc13ae16b7000001'),
+    name: 'Agna',
+    lastName: 'Polley',
+    email: 'apolley1@ebay.co.uk',
+    password: 'EGsryusXOawK',
+  },
+  {
+    _id: mongoose.Types.ObjectId('63533d49fc13ae16b7000002'),
+    name: 'Madelon',
+    lastName: 'Becken',
+    email: 'mbecken2@omniture.com',
+    password: 'IUqCYCI5',
+  },
+];
