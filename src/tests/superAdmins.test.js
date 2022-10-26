df465e6edbca8594f0de14b2ecbf74f9f14ff581
Julian Flores <julianfloresdev@gmail.com>
2022-10-26 19:00:48 -0300

    TG-31: Create GET all superAdmins tests

--- src/tests/superAdmins.test.js
+++ src/tests/superAdmins.test.js
@@ -7,6 +7,103 @@ beforeAll(async () => {
   await superAdmins.collection.insertMany(superAdminsSeed);
 });
 
+const superAdminValid = {
+  name: 'Verona',
+  lastName: 'Baudracco',
+  email: 'ReDesaparecidaLaVero@gmail.com',
+  password: 'NoEsVeronicaEsVERONA',
+};
+
+const superAdminInvalid = {
+  name: {
+    name: 'Vero NaShe',
+    lastName: 'Baudracco',
+    email: 'ReDesaparecidaLaVero@gmail.com',
+    password: 'NoEsVeronicaEsVERONA',
+  },
+  lastName: {
+    name: 'Verona',
+    lastName: 'Baudraggooooooool!!!!',
+    email: 'ReDesaparecidaLaVero@gmail.com',
+    password: 'NoEsVeronicaEsVERONA',
+  },
+  email: {
+    name: 'Verona',
+    lastName: 'Baudracco',
+    email: 'aparece Vero!!!@gmail.com',
+    password: 'NoEsVeronicaEsVERONA',
+  },
+  password: {
+    name: 'Verona',
+    lastName: 'Baudracco',
+    email: 'ReDesaparecidaLaVero@gmail.com',
+    password: 'LaVerónica//(//',
+  },
+  missingProp: {
+    name: 'Verona',
+    lastName: 'Baudracco',
+    email: 'ReDesaparecidaLaVero@gmail.com',
+  },
+};
+
+describe('GET all /api/superAdmins', () => {
+  describe('Success GET tests', () => {
+    test('if send a VALID path it should return a 200 status code', async () => {
+      const response = await request(app).get('/api/superAdmins').send();
+      expect(response.status).toBe(200);
+    });
+    test('if send a VALID path it should return a message like "Super Admin found"', async () => {
+      const response = await request(app).get('/api/superAdmins').send();
+      expect(response.body.message).toBe('Super Admin found');
+    });
+    test('if send an object with invalid properties on body it should return ALL Super Admins', async () => {
+      const response = await request(app).get('/api/superAdmins').send({ football: 'allways' });
+      expect(response.body.data.length).toBe(superAdminsSeed.length);
+    });
+    test('if send an object with invalid properties on body it should return 200 status code', async () => {
+      const response = await request(app).get('/api/superAdmins').send();
+      expect(response.status).toBe(200);
+    });
+    test('if send a valid superAdmin property on query params it should return this exactly superAdmin', async () => {
+      const response = await request(app).get('/api/superAdmins/?name=Verona').send();
+      expect(response.body.data).toMatchObject([superAdminValid]);
+    });
+    test('if send a valid superAdmin property on query params it should return those two superAdmins', async () => {
+      const response = await request(app).get('/api/superAdmins/?name=Verona&name=Ignacio').send();
+      expect(response.status).toBe(200);
+      expect(response.body.data.length).toBe(2);
+      expect(response.body.data).toMatchObject([{
+        name: 'Ignacio',
+        lastName: 'Aristo',
+        email: 'NoMeDigan_Nacho@gmail.com',
+        password: 'elMrSatanSinRulos',
+      },
+      {
+        name: 'Verona',
+        lastName: 'Baudracco',
+        email: 'ReDesaparecidaLaVero@gmail.com',
+        password: 'NoEsVeronicaEsVERONA',
+      }]);
+    });
+    test('if send an invalid superAdmin property on query params it should return a valid request with all superAdmins', async () => {
+      const response = await request(app).get('/api/superAdmins/?fulbo=yes').send();
+      expect(response.status).toBe(200);
+      expect(response.body.data.length).toBe(superAdminsSeed.length);
+    });
+  });
+  describe('Wrong GET tests', () => {
+    test('if send an INVALID path it should return a 404 status code', async () => {
+      const response = await request(app).get('/api/superDuperAdmins').send();
+      expect(response.status).toBe(404);
+    });
+    test('if send an INVALID query params it should return "Admin Not Found" with 404 status code', async () => {
+      const response = await request(app).get('/api/superAdmins/?name=Alberto&lastName=queMalPensado').send();
+      expect(response.status).toBe(404);
+      expect(response.body.message).toBe('Super Admin not found');
+    });
+  });
+});
+
 describe('GETBYID /api/superAdmins', () => {
   test('Existent ID response have to be 200', async () => {
     const response = await request(app).get('/api/superAdmins/6352d3e9239ac7eb3f7d3e88').send();
@@ -87,5 +184,33 @@ describe('DELETE-BY-ID /api/superAdmins', () => {
     const response = await request(app).del('/api/superAdmins/63534ef4fc13ae1a71000').send();
     expect(response.body.data).toBeUndefined();
     expect(response.body).toBeDefined();
+  });
+});
+
+describe('POST /api/superAdmins', () => {
+  describe('Success POST test', () => {
+    test('', async () => {});
+  });
+  describe('Wrong POST test', () => {
+    test('if send an invalid proeprty value it should return status code 400', async () => {
+      const response = await request(app).post('/api/superAdmins').send(superAdminInvalid.name);
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
+    test('if send an invalid proeprty value it should return status code 400', async () => {
+      const response = await request(app).post('/api/superAdmins').send(superAdminInvalid.lastName);
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
+    test('if send an invalid proeprty value it should return status code 400', async () => {
+      const response = await request(app).post('/api/superAdmins').send(superAdminInvalid.email);
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
+    test('if send an invalid proeprty value it should return status code 400', async () => {
+      const response = await request(app).post('/api/superAdmins').send(superAdminInvalid.passsword);
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
   });
 });
