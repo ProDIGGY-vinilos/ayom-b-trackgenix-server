0d78ca3a1b025768c7604a07a66fa34e2c14ec83
fede230298 <federicoperaltaquinteros@gmail.com>
2022-11-04 16:41:52 -0300

    TG-72: FIX Project validations

--- src/validations/projects.js
+++ src/validations/projects.js
@@ -4,14 +4,17 @@ const validateCreation = (req, res, next) => {
   const employeeValidation = Joi.object({
     employee: Joi.string().required(),
     role: Joi.string().valid('DEV', 'QA', 'PM', 'TL').required(),
-    rate: Joi.number().required(),
+    rate: Joi.number().positive().required(),
   });
   const projectValidation = Joi.object({
-    name: Joi.string().min(3).required(),
-    description: Joi.string().min(3).required(),
+    name: Joi.string().required().trim().regex(/^(?=.*[a-zA-Z].*)([\w\s\W]+)$/)
+      .min(3),
+    description: Joi.string().required().trim().regex(/^(?=.*[a-zA-Z].*)([\w\s\W]+)$/)
+      .min(3),
     startDate: Joi.date().required(),
-    endDate: Joi.date().required(),
-    clientName: Joi.string().min(3).required(),
+    endDate: Joi.date().greater(Joi.ref('startDate')).required(),
+    clientName: Joi.string().required().trim().regex(/^(?=.*[a-zA-Z].*)([\w\s\W]+)$/)
+      .min(3),
     employees: Joi.array().items(employeeValidation),
   });
 
